107b2f5e8d7361c9ae7fab85757e91d176778108
waashii <70151834+waashii@users.noreply.github.com>
2020-10-24 17:19:18 +0300

    خلصت للمرة المليون🙃

---
+++ TRYPage/try.js
@@ -0,0 +1,30 @@
+
+function qu() {
+    let answer = "put your hands in the air if you understand"  
+    if (document.getElementById("input").value == answer) {
+        document.getElementById("pa").innerHTML='👏 كفوانت فاهم😎'
+    }
+    else{
+        document.getElementById("pa").innerHTML='🤡 روح راجع عدل يا الذكي'
+    }
+}
+
+
+
+//      console.log(document.getElementById("").value)
+// }
+function qi(){
+    let answerr = "stay safe in your home and learn morse code"  
+    if (document.getElementById("inputt").value == answerr) {
+        document.getElementById("pb").innerHTML='👏 كفوانت فاهم😎'
+    }
+    else{
+        document.getElementById("pb").innerHTML='🤡 روح راجع عدل يا الذكي'
+    }
+
+
+
+}
+     console.log(document.getElementById("pb").value)
+// }
+
---
+++ homePage/script.js
@@ -0,0 +1,259 @@
+function myFunction() {
+  console.log("Hi");
+  let text = document.getElementById("p").value;
+
+
+
+let i = 0;
+     for(; i < text.length; i++) {
+    
+      let char = text.charAt(i);
+      let x = i*1000
+       console.log(char);
+       if(char == "A") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/A.mp3";
+      
+       setTimeout(function(){  bleep.play(); }, x+2000); 
+     
+      }     else if (char == "B") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/B.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+
+      }
+                
+      else if(char == "C") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/C.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+        
+     else if(char == "D") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/D.mp3";
+        //  setTimeout( bleep.play(), 3000);
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+        
+     else if(char == "E") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/E.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+        
+     else if(char == "F") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/F.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "G") {
+
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/G.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "H") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/H.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "I") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/I.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "J") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/J.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "K") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/K.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "L") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/L.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "M") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/M.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "N") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/N.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "O") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/O.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "P") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/P.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "Q") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/Q.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "R") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/R.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+
+    
+        
+     else if(char == "S") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/S.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+ 
+    
+        
+     else if(char == "T") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/T.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+  
+    
+        
+     else if(char == "U") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/U.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+
+    
+        
+     else if(char == "V") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/V.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      }
+
+    
+        
+     else if(char == "W") {
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/W.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      
+      }
+
+    
+        
+     else if(char == "X") {
+      
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/X.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+      
+      }
+
+    
+        
+     else if(char == "Y") {
+      
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/Y.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+    
+     }
+  
+    
+        
+     else if(char == "Z") {
+     
+        let bleep = new Audio();
+        bleep.src = "./homePage/voices/Z.mp3";
+        setTimeout(function(){  bleep.play(); }, x+2000);
+        // bleep.play()
+   
+}
+
+
+
+     }
+
+}
+
+function backg(){
+
+  let bg = new Audio();
+  bg.src = "./homePage/homebg.mp3";
+
+   bg.play()
+}
